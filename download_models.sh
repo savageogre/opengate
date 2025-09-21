25d5a42c833cf6d4d949a69cf272da5e3fa48aef
Savage Ogre <savageogre.music@gmail.com>
2025-09-20 19:10:04 -0700

    feat: download models

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
+modeldata=$(cat <<'EOF'
+en_US,amy,medium
+en_US,reza_ibrahim,medium
+EOF
+)
+
+function download-model() {
+    lang="$1"
+    name="$2"
+    size="$3"
+    url="https://huggingface.co/rhasspy/piper-voices/resolve/main/en/$lang/$name/$size/$lang-$name-$size.onnx"
+    lpath="./models/$lang-$name-$size.onnx"
+    echo "Getting $lpath from $url"
+    wget "$url" -O "$lpath"
+    wget "$url.json" -O "$lpath.json"
+}
+
 mkdir -p models
-wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx -O models/en_US-amy-medium.onnx
-wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx.json -O models/en_US-amy-medium.onnx.json
+echo "$modeldata" | while IFS=',' read -r lang name size; do
+    download-model "$lang" "$name" "$size"
+done
